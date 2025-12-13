ff4fb19f5da69d26d716cce6fcb603d006e70e37
Silvia <silvia940327@gmail.com>
2025-12-13 12:25:07 -0500

    initial structure for cafe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Portfolio from './Portfolio';
+import IndieBitesCafe from './IndieBitesCafe';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
