2608ac92a13b18fa8c0924154a65ad41d8636df9
Silvia <silvia940327@gmail.com>
2025-12-13 17:25:41 -0500

    minor change in color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import Navbar from './components/Navbar';
-import Header from './components/Header';
+import Hero from './components/Hero';
 
 
 function App() {
   return (
     <>
       <Navbar />
-      <Header />
+      <Hero />
     </>
   );
 }
