aa81be3913dead2a37b0d2ff4994954b85bdf337
Silvia <silvia940327@gmail.com>
2026-02-18 13:16:15 -0500

    api setting up for Render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OrderingPage from './pages/ordering/OrderingPage';
 import LandingPage from './pages/landing/LandingPage';
 
 
 function App() {
+  useEffect(() => {
+    const wakingBackend = async() => {
+      try{
+        const apiURL = process.env.REACT_APP_API_URL;
+        console.log("Pinging backend to wake up...");
+        await fetch(`${apiURL}/api/products`);
+        console.log("Backend is alive!");
+      }catch (e)
+      {
+        console.log("Backend still loading...")
+      }
+    };
+
+    wakingBackend();
+  }, [])
+
   return (
     <Router>
       <Routes>
