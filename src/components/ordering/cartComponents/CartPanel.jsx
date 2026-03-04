ce099b88fa907d5ed9ef79fde0983f871fb41302
Silvia <silvia940327@gmail.com>
2026-03-04 12:33:30 -0500

    cart panel, cart-item styling done, overlay added to allow for closing of panel when click outside (TBC)

--- src/components/ordering/cartComponents/CartPanel.jsx
+++ src/components/ordering/cartComponents/CartPanel.jsx
@@ -7,44 +7,49 @@ const CartPanel = ({ isOpen, onClose }) => {
     if (!isOpen) return null;
 
     return(
-        <div className="cart-panel-wrapper">
-            <div className="cart-panel">
-                <div className="cart-header">
-                    <h2>Your Cart</h2>
-                    <button className="close-cart-btn" onClick={onClose}>
-                        <IoClose size={30} />
-                    </button>
-                </div>
+        <div className ="cart-panel-overlay">
+            <div className="cart-backdrop" onClick={onClose}>
+                <div className="cart-panel-wrapper">
+                    <div className="cart-panel">
+                        <div className="cart-header">
+                            <h2>Your Cart</h2>
+                            <button className="close-cart-btn" onClick={onClose}>
+                                <IoClose size={30} />
+                            </button>
+                        </div>
 
-                <div className={`cart-content ${cartItems.length === 0 ? 'is-empty' : ''}`}>
-                    {cartItems?.length === 0 ? (
-                        <p>Your cart is empty.</p>
-                    ) : (
-                        cartItems.map((item)=> (
-                            <div key={item.orderItemId} className="cart-item-row">
-                                <div className="cart-item-pic">
-                                    <img src={item.product?.productImageURL} alt={item.product.productName} />
-                                </div>
-                                <div className="cart-item-info">
-                                    <span className="cart-item-name">{item.product?.productName} x {item.orderQty}</span>
-                                </div>
-                                <div className="cart-item-actions">
-                                    {/* Future implementation: allow update quantity */}
-                                    <button className="cart-remove-item-btn" onClick={() => removeFromCart(item.product?.productId)}>
-                                        <IoClose/>
-                                    </button>
-                                    {/* close button not functioning properly*/}
-                                </div>
-                            </div>
-                        ))
-                    )}
-                </div>
-                <div className="cart-footer">
-                    <button className="general-btn cancel-btn" onClick={onClose}>Cancel</button>
-                    <button className="general-btn checkout-btn">Checkout</button>
+                        <div className={`cart-content ${cartItems.length === 0 ? 'is-empty' : ''}`}>
+                            {cartItems?.length === 0 ? (
+                                <p>Your cart is empty.</p>
+                            ) : (
+                                cartItems.map((item)=> (
+                                    <div key={item.orderItemId} className="cart-item-row">
+                                        <div className="cart-item-pic">
+                                            <img src={item.product?.productImageURL} alt={item.product.productName} />
+                                        </div>
+                                        <div className="cart-item-info">
+                                            <span className="cart-item-name">{item.product?.productName} x {item.orderQty}</span>
+                                        </div>
+                                        <div className="cart-item-actions">
+                                            {/* Future implementation: allow update quantity */}
+                                            <button className="cart-remove-item-btn" onClick={() => removeFromCart(item.product?.productId)}>
+                                                <IoClose/>
+                                            </button>
+                                            {/* close button not functioning properly*/}
+                                        </div>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+                        <div className="cart-footer">
+                            <button className="general-btn cancel-btn" onClick={onClose}>Cancel</button>
+                            <button className="general-btn checkout-btn">Checkout</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        
     );
 };
 
